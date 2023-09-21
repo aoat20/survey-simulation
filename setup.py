88bab61aff8933857f93f1519da12a34493c26d1
aoat20 <145701687+aoat20@users.noreply.github.com>
2023-09-21 18:32:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     description='Simple survey simulation',
     url='https://github.com/aoat20/survey-simulation',
     license='Not yet licensed', 
-    packages=['survey-simulation'],
+    packages=['survey_simulation'],
     install_requires=['opencv-python', 
                       'matplotlib', 
                       'numpy'],
