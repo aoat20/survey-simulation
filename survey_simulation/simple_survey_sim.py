0d74af8be0b86271040bbf2a476b2946c0f10938
Alfie Anthony Treloar <aoat20@bath.ac.uk>
2023-09-25 17:35:31 +0100

    fix direction arrows not disappearing

--- survey_simulation/simple_survey_sim.py
+++ survey_simulation/simple_survey_sim.py
@@ -763,6 +763,8 @@ class SurveySimulation():
             # work out how many more contacts have been added and add new ones
             l_d = len(detections)
             l_p = len(self.detectionslineplt)
+            for n in range (l_p - l_d):
+                self.detectionslineplt.pop()
             for n in range(l_d - l_p):
                 self.detectionslineplt.append(plt.scatter(detections[l_p+n].x,
                                                           detections[l_p+n].y,
