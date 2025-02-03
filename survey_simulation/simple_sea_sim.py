d4f5b36680ba2fe30a37f336da7095e78a5c3d9f
Alfie Anthony Treloar <aoat20@bath.ac.uk>
2025-02-03 15:46:10 +0000

    Added more scenarios

--- survey_simulation/simple_sea_sim.py
+++ survey_simulation/simple_sea_sim.py
@@ -3,6 +3,7 @@ from survey_simulation.sim_plotter import SEASPlotter
 from survey_simulation.sim_classes import Timer, Playback, Logger, Map, Agent
 import json
 import pyproj
+import os
 from matplotlib.widgets import TextBox, Button
 
 
@@ -21,34 +22,45 @@ class SEASSimulation():
         self.speed_reached = True
 
         # Load the desired scene
-        params, self._agent, self._vessels, xy_lim = self._load_scene(
-            f"SEASscenario{scenario_n}.json")
+        scen_pth = os.path.join('SEA_scenarios',
+                                f"SEASscenario{scenario_n}.json")
+        params, self._agent, self._vessels, xy_lim = self._load_scene(scen_pth)
 
         self._timer = Timer(time_lim=60*60,
-                            t_step=0.1)
+                            t_step=params["t_step"])
 
         if plotter:
-            plotter_obj = SEASPlotter(map_lims=([sum(x) for x in zip(xy_lim,
-                                                                     [-10000,
-                                                                      10000,
-                                                                      -10000,
-                                                                      10000])]),
-                                      agent=self._agent,
-                                      vessels=self._vessels)
-            pos_tmp = plotter_obj.ax.get_position()
-            plotter_obj.ax.set_position([pos_tmp.x0,
-                                        pos_tmp.y0,
-                                        pos_tmp.width,
-                                        pos_tmp.height*0.9])
+            self._plotter_obj = SEASPlotter(map_lims=([sum(x)
+                                                       for x in zip(xy_lim,
+                                                                    [-10000,
+                                                                     10000,
+                                                                     -10000,
+                                                                     10000])]),
+                                            agent=self._agent,
+                                            vessels=self._vessels)
+            if mode == 'manual':
+                pos_tmp = self._plotter_obj.ax.get_position()
+                self._plotter_obj.ax.set_position([pos_tmp.x0,
+                                                  pos_tmp.y0,
+                                                  pos_tmp.width,
+                                                  pos_tmp.height*0.9])
 
-            self._add_controls(plotter_obj)
-            self._plotting_loop(plotter_obj)
+                self._add_controls()
+                self._plotting_loop()
 
     def get_obs(self):
-        obs_dict = {"agent_speed": self._agent.speed,
-                    "agent_course": self._agent.course,
-                    "agent_coords": self._agent.xy,
-                    "vessel_speeds": [v.speed for v in self._vessels]}
+        obs_dict = {}
+        obs_dict['agent'] = {"speed": self._agent.speed,
+                             "course": self._agent.course,
+                             "coords_utm": self._agent.xy}
+        for v in self._vessels:
+            obs_dict[v.vessel_type] = {"speed": v.speed,
+                                       "course": v.course,
+                                       "coords_utm": v.xy,
+                                       "cpa_yds": v.cpa_yds,
+                                       "tcpa_s": v.tcpa_s}
+
+        return obs_dict
 
     def set_course(self,
                    course):
@@ -95,93 +107,102 @@ class SEASSimulation():
         yds = m*1.09361
         return yds
 
-    def _add_controls(self,
-                      plotter: SEASPlotter):
+    def _add_controls(self):
         # Controls callback
-        plotter.ax.figure.canvas.mpl_connect('key_press_event',
-                                             self._on_key)
+        self._plotter_obj.ax.figure.canvas.mpl_connect('key_press_event',
+                                                       self._on_key)
         # Course
-        course_ax = plotter.fig.add_axes([0.25, 0.9, 0.1, 0.075])
+        course_ax = self._plotter_obj.fig.add_axes([0.25, 0.9, 0.1, 0.075])
         self._course_req_box = TextBox(course_ax,
                                        "Course change (deg)",
                                        textalignment="left")
         self._course_req_box.set_val(f"{self._agent.course:.1f}")
         self._course_req_box.on_submit(self._change_course)
-        ax_c_pl1 = plotter.fig.add_axes([0.35, 0.9375, 0.05, 0.0375])
+        ax_c_pl1 = self._plotter_obj.fig.add_axes([0.35, 0.9375, 0.05, 0.0375])
         self._b_c_pl1 = Button(ax_c_pl1, '+1')
         self._b_c_pl1.on_clicked(self._course_add_one)
-        ax_c_pl2 = plotter.fig.add_axes([0.35, 0.9, 0.05, 0.0375])
+        ax_c_pl2 = self._plotter_obj.fig.add_axes([0.35, 0.9, 0.05, 0.0375])
         self._b_c_pl2 = Button(ax_c_pl2, '-1')
         self._b_c_pl2.on_clicked(self._course_minus_one)
 
         # Speed
-        speed_ax = plotter.fig.add_axes([0.7, 0.9, 0.1, 0.075])
+        speed_ax = self._plotter_obj.fig.add_axes([0.7, 0.9, 0.1, 0.075])
         self._speed_req_box = TextBox(speed_ax,
                                       "Speed change (kn)",
                                       textalignment="left")
         ag_sp_kn = self._agent.speed*1.944
         self._speed_req_box.set_val(f"{ag_sp_kn:.1f}")
         self._speed_req_box.on_submit(self._change_speed)
-        ax_s_pl1 = plotter.fig.add_axes([0.8, 0.9375, 0.05, 0.0375])
+        ax_s_pl1 = self._plotter_obj.fig.add_axes([0.8, 0.9375, 0.05, 0.0375])
         self._b_s_pl1 = Button(ax_s_pl1, '+1')
         self._b_s_pl1.on_clicked(self._speed_add_one)
-        ax_s_pl2 = plotter.fig.add_axes([0.8, 0.9, 0.05, 0.0375])
+        ax_s_pl2 = self._plotter_obj.fig.add_axes([0.8, 0.9, 0.05, 0.0375])
         self._b_s_pl2 = Button(ax_s_pl2, '-1')
         self._b_s_pl2.on_clicked(self._speed_minus_one)
 
         # Play speed
-        ax_playspeed1 = plotter.fig.add_axes([0.65, 0.82, 0.05, 0.05])
+        ax_playspeed1 = self._plotter_obj.fig.add_axes(
+            [0.65, 0.82, 0.05, 0.05])
         self._playspeed_button1 = Button(ax_playspeed1, 'RT')
         self._playspeed_button1.on_clicked(self._playspeed_rt)
-        ax_playspeed2 = plotter.fig.add_axes([0.7, 0.82, 0.05, 0.05])
+        ax_playspeed2 = self._plotter_obj.fig.add_axes([0.7, 0.82, 0.05, 0.05])
         self._playspeed_button2 = Button(ax_playspeed2, 'x2')
         self._playspeed_button2.on_clicked(self._playspeed_x2)
-        ax_playspeed3 = plotter.fig.add_axes([0.75, 0.82, 0.05, 0.05])
+        ax_playspeed3 = self._plotter_obj.fig.add_axes(
+            [0.75, 0.82, 0.05, 0.05])
         self._playspeed_button3 = Button(ax_playspeed3, 'x10')
         self._playspeed_button3.on_clicked(self._playspeed_x10)
 
-    def _plotting_loop(self,
-                       plotter_obj: SEASPlotter):
-        plotter_obj.show(blocking=False)
+    def _plotting_loop(self):
+        self._plotter_obj.show(blocking=False)
 
         self._play = True
         self._ps_change = False
 
         while True:
             if self._play and not self.mission_failed:
-                self.next_step()
-                self._update_plot(plotter_obj)
+                self._next_step_manual()
                 # Control handlers
                 if self._ps_change:
                     self._ps_change = False
-                    plotter_obj.updateps(self._playspeed)
+                    self._plotter_obj.updateps(self._playspeed)
                 if self.mission_failed:
-                    plotter_obj.explode(self._agent.xy)
+                    self._plotter_obj.explode(self._agent.xy)
 
-            plotter_obj.pause(self._timer.t_step/self._playspeed)
+            self._plotter_obj.pause(1/25)
 
     def next_step(self):
+        self._adv_time(self._timer.t_step)
+
+    def _next_step_manual(self):
+        t = self._timer.t_step*self._playspeed/25
+        self._adv_time(t)
+
+    def _adv_time(self, t):
         if not self.mission_failed:
-            self._timer.update_time(self._timer.t_step*self._playspeed)
-            self._agent.advance_one_step(self._timer.t_step*self._playspeed)
+            self._timer.update_time(t)
+            self._agent.advance_one_step(t)
             for v in self._vessels:
-                v.advance_one_step(self._timer.t_step*self._playspeed)
+                v.advance_one_step(t)
                 v.cpa_yds, v.tcpa_s = self._compute_cpa(self._agent.xy,
                                                         self._agent.course,
                                                         self._agent.speed,
                                                         v.xy,
                                                         v.course,
                                                         v.speed)
-            self._check_failure_conditions()
 
-    def _update_plot(self,
-                     plotter_obj: SEASPlotter):
-        plotter_obj.update_minutecounter(self._timer.time_elapsed)
-        self._update_agentplots(plotter_obj.agent,
+            self._check_failure_conditions()
+            if hasattr(self, '_plotter_obj'):
+                self._update_plot()
+                self._plotter_obj.pause(0.0001)
+
+    def _update_plot(self):
+        self._plotter_obj.update_minutecounter(self._timer.time_elapsed)
+        self._update_agentplots(self._plotter_obj.agent,
                                 self._agent)
         n = 0
         for v in self._vessels:
-            self._update_agentplots(plotter_obj.vessels[n],
+            self._update_agentplots(self._plotter_obj.vessels[n],
                                     v)
             n += 1
 
@@ -210,7 +231,7 @@ class SEASSimulation():
                         ellps='WGS84',
                         preserve_units=False)
 
-        grid_conv = 1.6
+        # grid_conv = 1.6
 
         # Open and load the config file
         f = open(config_file)
@@ -231,22 +252,24 @@ class SEASSimulation():
             xy_lim_tmp.extend(way_points)
 
             speed_mps = 0.5144*v["speed"]
-            course = v["course"]
+            # course = v["course"]+grid_conv
+            course = np.rad2deg(np.arctan2(way_points[1][0]-way_points[0][0],
+                                           way_points[1][1]-way_points[0][1]))
             # AI agent
-            if v['vessel'] == "CandidateMass":
+            if v['vessel'] == "agent":
                 agent = Agent(xy_start=way_points[0],
                               speed=speed_mps,
-                              course=course+grid_conv,
+                              course=course,
                               vessel_type='agent',
                               waypoints=way_points)
                 agent.course_change_rate = v["turning_rate"]
                 agent.speed_change_rate = v["speed_change_rate"]
                 # agent.speed_max = v['max_speed']*0.5144
-            elif v['vessel'] == "CruiseLiner":
+            else:
                 vessels.append(Agent(xy_start=way_points[0],
                                      speed=speed_mps,
-                                     course=course+grid_conv,
-                                     vessel_type='cruise liner',
+                                     course=course,
+                                     vessel_type=v['vessel'],
                                      waypoints=way_points))
         f.close()
 
@@ -323,11 +346,8 @@ class SEASSimulation():
         elif event.key == "down":
             self._playspeed = self._playspeed/1.5
             self._ps_change = True
-        elif event.key == 'enter':
-            self.reset()
         elif event.key == " ":
             self._play = not self._play
-
         elif event.key == "enter":
             self._reset()
 
