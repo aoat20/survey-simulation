cf907098e4bc91ba53bbb6ab3d818fc7af516409
Alfie Anthony Treloar <aoat20@bath.ac.uk>
2025-07-15 11:39:32 +0100

    Updated example script with new set_waypoint function

--- BMT_example_script.py
+++ BMT_example_script.py
@@ -1,11 +1,11 @@
 from survey_simulation import SEASSimulation
 
 # To run scenario 4 in manual mode
-ss = SEASSimulation(scenario_n=4,
+ss = SEASSimulation(scenario_n=2,
                     mode='manual')
 
 # To set up scenario 4 in test mode
-ss = SEASSimulation(scenario_n=4,
+ss = SEASSimulation(scenario_n=2,
                     mode='test',
                     plotter=True)
 
@@ -18,11 +18,14 @@ while not ss.mission_finished:
     obs = ss.get_obs()
 
     # Do stuff based on the observations
-    if obs['time_s'] == 200:
+    if obs['time_s'] == 300:
+        ss.set_waypoints([[420_000, 5_560_000],
+                          [425_000, 5_560_000]])
+    if obs['time_s'] == 2000:
         ss.set_course(30)
-    if obs['time_s'] == 240:
+    if obs['time_s'] == 2400:
         ss.set_speed(30)
-    if obs['time_s'] > 350 and ss.course_reached:
+    if obs['time_s'] > 3500 and ss.course_reached:
         ss.set_course(-60)
 
 print(ss.termination_reason)
